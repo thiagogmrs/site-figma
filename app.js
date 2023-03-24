0438afcace6b2aa788e653daaccd60bf7fa5af83
Thiago Guimarães <104789081+thiagogmrs@users.noreply.github.com>
2023-03-24 17:34:32 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const btnMobile = document.querySelector('.btn-mobile');
+let btnmobile = document.querySelector("#btn-mobile");
 
-btnMobile.addEventListener('click', ()=>{
-      window.scrollTo(0,0);
-});
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    btnmobile.style.display = "flex"; 
+  } else {
+    btnmobile.style.display = "none"; 
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
